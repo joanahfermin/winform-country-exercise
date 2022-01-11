0d1bd6a95dda26d9f58e1098c9bf1df262de9e71
joanahfermin <ferminjoanah@gmail.com>
2022-01-11 20:03:03 +0800

    Created project and added simple layout
    Created a new project and added a simple GUI layout with list view to show table contents, and textbox with Add button for adding new records

---
+++ winform-country-exercise/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace winform_country_exercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvCountry = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbCountryName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lvCountry
+            // 
+            this.lvCountry.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lvCountry.HideSelection = false;
+            this.lvCountry.Location = new System.Drawing.Point(39, 29);
+            this.lvCountry.Name = "lvCountry";
+            this.lvCountry.Size = new System.Drawing.Size(715, 298);
+            this.lvCountry.TabIndex = 0;
+            this.lvCountry.UseCompatibleStateImageBehavior = false;
+            this.lvCountry.View = System.Windows.Forms.View.List;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "CountryID";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Country Name";
+            this.columnHeader2.Width = 150;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(595, 359);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(159, 26);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tbCountryName
+            // 
+            this.tbCountryName.Location = new System.Drawing.Point(39, 359);
+            this.tbCountryName.Name = "tbCountryName";
+            this.tbCountryName.Size = new System.Drawing.Size(537, 26);
+            this.tbCountryName.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbCountryName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lvCountry);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvCountry;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tbCountryName;
+    }
+}
+
